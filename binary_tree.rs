3f706e261cd06c1cc1ade313fd80d9da88c9149c
唐璜铂金 <donjuan@donjoo.com>
2023-12-23 22:38:21 +0800

    显示标所有的解引用

--- binary_tree.rs
+++ binary_tree.rs
@@ -1,11 +1,23 @@
 pub struct TreeNode<T> {
-    value: T,
+    value: Option<T>,
     left: Option<std::sync::Arc<TreeNode<T>>>,
     right: Option<std::sync::Arc<TreeNode<T>>>,
+    count: i32,
 }
 
 
+pub trait BinaryTree {
+    fn new<T>() -> TreeNode<T>;
+    fn get_left<T>(&self) -> TreeNode<T>;
+    fn get_right<T>(&self) -> TreeNode<T>;
+    fn get_parent<T>(&self) -> TreeNode<T>;
+}
 
+fn statistic_tree<T: std::cmp::PartialEq>(a : &[T]) ->() {
+    let root_tree: TreeNode<T>;
+    
+
+}
 fn main() {
     
 }
--- insert_insort.rs
+++ insert_insort.rs
@@ -1,9 +1,9 @@
 fn insert_insort<T: std::cmp::PartialOrd>(arr: &mut[T]) {
-    let len = arr.len();
+    let len = (*arr).len();
     for index in 0..len {
         let mut i = index ;
         while i >0 && arr[i] < arr[i - 1] {
-            arr.swap(i,i - 1);
+            (*arr).swap(i,i - 1);
             i = i - 1;
         }
     }

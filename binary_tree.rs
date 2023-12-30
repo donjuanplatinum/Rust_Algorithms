1aa98698f9e520edb23796c8d3f7e0cdeaebf891
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-30 13:52:59 +0800

    更新二叉树

--- binary_tree.rs
+++ binary_tree.rs
@@ -5,31 +5,17 @@ pub struct TreeNode<T> {
     count: Option<i32>,
 }
 
-
-
-
-fn statistic_tree<T: std::cmp::PartialEq + std::clone::Clone>(a : &[T]) ->() {
-    let mut root_tree: TreeNode<T> = TreeNode { value: Option::<T>::None ,
-                                            left: Option::<std::sync::Arc<TreeNode<T>>>::None,
-                                            right: Option::<std::sync::Arc<TreeNode<T>>>::None,
-                                            count: Option::<i32>::None };
-    for elements in a {
-        add_tree(&mut root_tree, elements);
+impl<T> TreeNode<T> {
+    fn new(value: T) -> Self {
+        TreeNode{
+            value: Option::None,
+            left: Option::None,
+            right: Option::None,
+            count: Option::None,
+        }
     }
-
-
 }
 
-fn add_tree<T: std::cmp::PartialEq + std::clone::Clone>(a: &mut TreeNode<T>, b: &T) {
-    if (*a).value == Option::None {
-        let newtree: TreeNode<T> = TreeNode::<T> {
-            value: Option::<T>::Some(b.clone()),
-            left: Option::<std::sync::Arc<TreeNode<T>>>::None,
-            right: Option::<std::sync::Arc<TreeNode<T>>>::None,
-            count: Option::<i32>::Some(1),
-        };
-    } else  
-}
 fn main() {
     
 }

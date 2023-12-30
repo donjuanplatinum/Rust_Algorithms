5e738c466a99fc0a42ced3cf1cb8df765ce11201
donjuanplatinum <donjuan@donjuan.lecturify.net>
2023-12-30 13:44:47 +0800

    完善分治排序

--- merge_insort.rs
+++ merge_insort.rs
@@ -1,52 +1,46 @@
-fn merge(a: &[i32], p:usize ,q: usize, r: usize ) -> Vec<i32> {
-    let n1 = q - p + 1;
-    println!("this is n1={}",n1);
-    let n2 = r - q ;
-    println!("this is n2={}", n2);
-    let b  = &a[p..=q];
-    let c  = &a[q+1..=r];
-    println!("this is b {:?}", b);
-    println!("this is c {:?}", c);
-    let mut i: usize = 0;
-    let mut j: usize = 0;
-    let mut d: Vec<i32> = vec!();
-    for k in p..=r {
-        println!("this is k={}", k);
-        println!("this is i = {} j = {}",i, j);
-        println!("this is b[i] and c[j] {} {}", b[i], c[j]);
-        if k == r - 1  {
-            if b[i] >= c[j] {
-                d.push(c[j]);
-                d.push(b[i]);
-            }
-            else {
-                d.push(b[i]);
-                d.push(c[j]);
-            }
-        break;
-        } else if b[i] >= c[j] {
-            d.push(c[j]);
-            if j < n2  {
-                j += 1;
-            } else {i += 1;}
+fn merge(left: Vec<i32>, right: Vec<i32>) -> Vec<i32> {
+    let mut result = Vec::new();
+    let mut left_index = 0;
+    let mut right_index = 0;
+
+    while left_index < left.len() && right_index < right.len() {
+        if left[left_index] < right[right_index] {
+            result.push(left[left_index]);
+            left_index += 1;
         } else {
-            d.push(b[i]);
-            if i < n1 {
-                i += 1;
-            } else {j += 1;}
+            result.push(right[right_index]);
+            right_index += 1;
         }
     }
-    println!("this is vector d ={:?}", d);
-    d
+
+    while left_index < left.len() {
+        result.push(left[left_index]);
+        left_index += 1;
+    }
+
+    while right_index < right.len() {
+        result.push(right[right_index]);
+        right_index += 1;
+    }
+
+    result
 }
 
-fn main(){
-    let a: [i32;10] = [1,2,3,4,5,2,3,4,5,6];
-    let p: Vec<i32> = merge(&a, 0, 4, 9);
-    assert_eq!(p,[1,2,2,3,3,4,4,5,5,6]);
+fn merge_sort(arr: &mut Vec<i32>) -> Vec<i32> {
+    if arr.len() <= 1 {
+        return arr.clone();
+    }
+
+    let mid = arr.len() / 2;
+    let left = merge_sort(&mut arr[0..mid].to_vec());
+    let right = merge_sort(&mut arr[mid..].to_vec());
+    merge(left, right)
 }
 
+fn main() {
+    let mut arr = vec![38, 27, 43, 3, 9, 82, 10];
+    println!("Original array: {:?}", arr);
 
-//array:  1 1 1 1 1 | 2 4 5 | 2 1 3 6 2
-//index:  0 1 2 3 4   5 6 7   8 9 10 11 12
-//
+    let sorted_arr = merge_sort(&mut arr);
+    println!("Sorted array: {:?}", sorted_arr);
+}

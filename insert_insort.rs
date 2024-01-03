5114295496a47b0bbf2cc5bd1a1f291246368611
donjuanplatinum <donjuan@donjuan.lecturify.net>
2024-01-03 23:16:26 +0800

    修复插入排序

--- insert_insort.rs
+++ insert_insort.rs
@@ -1,11 +1,11 @@
 
 fn insert_insort<T: std::cmp::PartialOrd>(arr: &mut[T]) {
     let len = (*arr).len();
-    for index in 0..=len {
+    for index in 1..len {
         let mut i = index ;
-        while i >0 && arr[i] <= arr[i - 1] {
+        while i > 0 && arr[i - 1] >= arr[i] {   
             (*arr).swap(i,i - 1);
-            i = i - 1;
+            i -= 1;
         }
     }
 }
@@ -13,6 +13,7 @@ fn insert_insort<T: std::cmp::PartialOrd>(arr: &mut[T]) {
 
 fn main() {
     let mut arr: [i32;5] = [1, 6, 8, 3, 2];
+    let mut arr1: [i32;5] = arr.clone();
     insert_insort::<i32>(&mut arr);
     assert_eq!(arr,[1, 2, 3, 6, 8]);
 }

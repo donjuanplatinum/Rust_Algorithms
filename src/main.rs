a5c15a3d951f8b7b86c9eababdd3d2bfad2e030a
donjuanplatinum <donjuan@donjuan.lecturify.net>
2024-01-28 23:34:28 +0800

    cargo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,2 @@
 fn main() {
-    let signal = vec![0.0, 1.0, 0.0, 1.0, 7.0 ,9.0]; // Input signal
-    let spectrum = crate::math::dft(&signal);
-    
-    for (k, value) in spectrum.iter().enumerate() {
-            println!("Frequency {}: {:?}", k, value);
-	}	
 }

5fad403a89ea55e55f9b949f7ce7dc64080b61c4
donjuanplatinum <donjuan@donjuan.lecturify.net>
2024-01-30 22:43:34 +0800

    矩阵

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    use algori::matrix::Matrix;
+    use algorithm_rust::matrix::Matrix;
     let matrix1 = Matrix::new(5, 10, vec![
         vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
         vec![11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0],
